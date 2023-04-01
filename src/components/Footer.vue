7d75488ee107ce051443c282642121e6a5a5f4c4
Adejuwon Oshadipe <bibire95@gmail.com>
2023-04-01 17:16:10 +0100

    feat: home page completed 100%

---
+++ src/components/Footer.vue
@@ -0,0 +1,40 @@
+<template>
+    <footer class="py-4 w-full bg-neutral-900 px-28 font-serif">
+        <div class="flex gap-4 justify-between items-center py-8">
+            <div> 
+                <img src="@/assets/media/logo/logo.svg" alt="">
+            </div>
+            <div class="text-white text-center">
+                <ul class="flex gap-6 text-lg">
+                    <li>FAQ</li>
+                    <li>Results</li>
+                    <li>Terms and Conditions</li>
+                </ul>
+            </div>
+            <div>
+                <div class="mb-2">
+                    <p>Our E-mail</p>
+                    <p class="text-zinc-600">support@mycroft.com</p>
+                </div>
+                <div>
+                    <p>Stay in touch</p>
+                    <div class="flex gap-2 mt-1">
+                        <img src="@/assets/media/icons/twitter.svg" alt="">
+                        <img src="@/assets/media/icons/facebook.svg" alt="">
+                        <img src="@/assets/media/icons/instagram.svg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="flex justify-center items-center gap-1 py-4">
+            <img src="@/assets/media/icons/copyright.svg" alt="">
+            <p>mycroft 2022 All rights reserved</p>
+        </div>
+    </footer>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
